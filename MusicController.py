5b15f41e653e83a886206879f685263f53695518
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-19 23:58:23 +0530

    Initialize pygame for the GUI

--- MusicController.py
+++ MusicController.py
@@ -7,3 +7,12 @@ import os
 # Initialize the mixer module in pygame
 pygame.mixer.init()
 
+# Initialize pygame for the GUI
+pygame.init()
+WIDTH = 350
+HEIGHT = 150
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Voice controlled music player")
+font1 = pygame.font.Font(None, 45)
+font2 = pygame.font.Font(None, 30)
+
